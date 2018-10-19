cd8b5e5f67a3236a19e2c6f602538aa9753f63cc
jh0904 <464062919@qq.com>
2018-10-19 17:18:00 +0800

    新添功能：1.实现员工修改 2.实现员工删除，

--- springboot-04-restfulcrud/src/main/java/com/jh/springboot04restfulcrud/controller/EmployeeController.java
+++ springboot-04-restfulcrud/src/main/java/com/jh/springboot04restfulcrud/controller/EmployeeController.java
@@ -7,7 +7,7 @@ import com.jh.springboot04restfulcrud.entities.Employee;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Collection;
 
@@ -24,12 +24,12 @@ public class EmployeeController {
 	DepartmentDao departmentDao;
 
 	@Autowired
-	EmployeeDao employee;
+	EmployeeDao employeeDao;
 
 	//查询所有员工
 	@GetMapping("/emps")
 	public String  list(Model model){
-		Collection<Employee> employees = employee.getAll();
+		Collection<Employee> employees = employeeDao.getAll();
 
 		//放在请求域中
 		model.addAttribute("emps",employees);
@@ -44,5 +44,38 @@ public class EmployeeController {
 		Collection<Department> departments = departmentDao.getDepartments ();
 		model.addAttribute ("depts",departments);
 		return "emp/add";
+	}
+	//添加员工
+	@PostMapping("/emp")
+	public String addEmp(Employee employee){
+		//插入完成返回员工列表页面   /代表当前项目路径
+		//redirect：重定向
+		//forward:转发
+		System.out.println ("--->"+employee);
+		employeeDao.save (employee);
+		return "redirect:/emps";
+	}
+	//查找指定的员工
+	@GetMapping("/emp/{id}")
+	public String toEditPage(@PathVariable("id") Integer id,Model model){
+		Employee employee = employeeDao.get (id);
+		model.addAttribute ("emp",employee);
+		Collection<Department> departments = departmentDao.getDepartments ();
+		model.addAttribute ("depts",departments);
+		return "emp/add";
+	}
+	//员工修改
+	@PutMapping("/emp")
+	public String update(Employee employee){
+		System.out.println ("修改员工数据--->"+employee);
+		employeeDao.save (employee);
+		return "redirect:/emps";
+	}
+	//员工删除
+	@DeleteMapping("/emp/{id}")
+	public String daleteEmployee(@PathVariable("id") Integer id){
+		System.out.println ("------->"+id);
+		employeeDao.delete (id);
+		return "redirect:/emps";
 	}
 }
